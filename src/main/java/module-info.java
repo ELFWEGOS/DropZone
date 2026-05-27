bf3d6f9fa4b02c66726b9370135bce6bdd90ee9f
ELFWEGOS <mizanmazin49@gmail.com>
2026-05-27 20:25:44 +0100

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,14 +5,15 @@ module com.elfwegos.dropezone {
     requires org.kordamp.ikonli.antdesignicons;
     requires org.kordamp.ikonli.javafx;
     requires com.google.gson;
+    requires jdk.compiler;
 
     opens com.elfwegos.dropezone to javafx.fxml, com.google.gson;
     exports com.elfwegos.dropezone;
     exports com.elfwegos.dropezone.controllers;
-    opens com.elfwegos.dropezone.controllers to javafx.fxml;
     exports com.elfwegos.dropezone.services;
     opens com.elfwegos.dropezone.services to javafx.fxml;
     opens com.elfwegos.dropezone.models to com.google.gson;
     opens com.elfwegos.dropezone.utils to com.google.gson;
+    opens com.elfwegos.dropezone.controllers to com.google.gson, javafx.fxml;
 
 }
