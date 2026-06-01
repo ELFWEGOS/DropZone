fef5901ef1abbfade1343d79fb9de899e760d7fa
ELFWEGOS <mizanmazin49@gmail.com>
2026-06-01 21:17:02 +0100

    fixed the new_dropzone starter

--- src/main/java/com/elfwegos/dropezone/controllers/UpdatePopUpController.java
+++ src/main/java/com/elfwegos/dropezone/controllers/UpdatePopUpController.java
@@ -1,7 +1,9 @@
 package com.elfwegos.dropezone.controllers;
 
 import com.elfwegos.dropezone.App;
+import com.elfwegos.dropezone.services.LogsManager;
 import com.elfwegos.dropezone.services.UpdateService;
+import com.elfwegos.dropezone.utils.LogTypes;
 import com.sun.tools.javac.Main;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -29,7 +31,7 @@ public class UpdatePopUpController {
     private Button yesButton;
 
     UpdateService updateService = UpdateService.getInstance();
-
+    LogsManager logsManager = LogsManager.getInstance();
     @FXML
     private void initialize() {
         String cloudVersion = String.valueOf(updateService.getCloudVersion());
@@ -39,7 +41,8 @@ public class UpdatePopUpController {
     public void onYesButtonClicked(ActionEvent event) throws URISyntaxException {
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Stage primaryStage = (Stage) stage.getOwner();
-        URI uri = App.class.getProtectionDomain()
+        URI uri = null;
+        uri = App.class.getProtectionDomain()
                 .getCodeSource()
                 .getLocation()
                 .toURI();
@@ -48,12 +51,17 @@ public class UpdatePopUpController {
         String batPath = pathDeLancement.resolve("startUpdater.bat").toString();
         ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c",batPath );
         processBuilder.directory(pathDeLancement.toFile());
-        try{
+
+        try {
+            logsManager.addLog(LogTypes.INFO,"lancement de l'updater");
             Process process = processBuilder.start();
-        }catch (IOException ignored){}
+            logsManager.addLog(LogTypes.SUCCESS,"updater lancé");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        logsManager.addLog(LogTypes.INFO,"APP CLOSED");
         primaryStage.close();
         Platform.exit();
-        System.exit(0);
     }
     public void onNoButtonClicked(ActionEvent event){
         Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
